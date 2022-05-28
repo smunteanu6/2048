e9537e159f1c499d9a9169b975c488051e84bf0b
Stefan Munteanu <smunteanu0206@gmail.com>
2022-05-28 22:11:27 +0300

    Added explanation

--- src/js/Render.js
+++ src/js/Render.js
@@ -3,6 +3,10 @@ import $ from '@lib/CreateElement';
 function Render () {
 
   const placeholderGrid = document.getElementById('placeholder-grid');
+
+  /*
+    <div class="cell" position={position} />
+  */
   
   for (const position in [...Array(16)]) placeholderGrid.append($('div', { className: 'cell', position }))
 
